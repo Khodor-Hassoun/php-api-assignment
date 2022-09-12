6bd41ce7551a95d6f8ef02653f51e2060c52785c
Khodor Hassoun <khodorhassoun@outlook.com>
2022-09-13 01:02:20 +0300

    Finish password api post request

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     $a = $_POST['a'];
     $b = $_POST['b'];
     $c = $_POST['c'];
+    $password = $_POST['password'];
 
     // API 1 RETURNS TRUE OR FALSE
     if($string == strrev($string)){
@@ -14,11 +15,19 @@
     //API 2 CALCULATES THE NUMBERS
     $abc = pow($a,3) + ($b*$c) - ($a/$b);
 
+    //API 3 STRONG PASSWORD
+    if(strlen($password) >12){
+        $isStrong = true;
+    }
+    else{
+        $isStrong = false;
+    }
 
 
     $results =[
         'isPalindrome' => $isPalindrome,
-        'abc' => $abc
+        'abc' => $abc,
+        'isStrong' => $isStrong
     ];
     echo json_encode($results);
 ?>

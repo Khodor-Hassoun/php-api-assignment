58693131aa50acd3b71fba78841b8cf230a5cac4
Khodor Hassoun <khodorhassoun@outlook.com>
2022-09-13 00:55:16 +0300

    Finish Second api post request

--- index.php
+++ index.php
@@ -1,13 +1,24 @@
 <?php
     // echo 'Hello world';
     $string = $_GET['string'];
+    $a = $_POST['a'];
+    $b = $_POST['b'];
+    $c = $_POST['c'];
+
+    // API 1 RETURNS TRUE OR FALSE
     if($string == strrev($string)){
         $isPalindrome=true;
     }else{
         $isPalindrome=false;
     }
+    //API 2 CALCULATES THE NUMBERS
+    $abc = pow($a,3) + ($b*$c) - ($a/$b);
+
+
+
     $results =[
-        'isPalindrome' => $isPalindrome
+        'isPalindrome' => $isPalindrome,
+        'abc' => $abc
     ];
     echo json_encode($results);
 ?>
